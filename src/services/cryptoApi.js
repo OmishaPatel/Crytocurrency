a50677c8f76a1cfc7a65b5bb5d2b983e687bc408
Omisha Patel <omishapatel@gmail.com>
2021-09-22 14:50:25 -0400

    remove api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "a80c9b7b55msh39a94a207f6e9cap144153jsnfe468b261c4b",
+  "x-rapidapi-key": 
 };
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({
